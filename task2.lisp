2bcff2a71cc2ac5a0628516e861732bbd5a0cec4
budetfkussno <65557657+budetfkussno@users.noreply.github.com>
2020-05-23 17:20:15 +0300

    Update task2.lisp
    0

--- task2.lisp
+++ task2.lisp
@@ -1,6 +1,17 @@
-;; task 2 reading file
-(let ((in (open "something.txt" :if-does-not-exist nil)))
-    (when in
-        (loop for line = (read-line in nil)
-    while line do (format t "~a~%" line))
-    (close in)))
+(defun output_file (path)
+    (let ((in (open path :if-does-not-exist nil)))
+        (when in
+            (loop for line = (read-line in nil)
+         while line do (format t "~a~%" line))
+            (close in)
+         )
+     )
+)
+
+(defun path() `("./something.txt"))
+
+(format t "~a~%" (path))
+
+(set `something_path (path))
+
+(output_file something_path)
